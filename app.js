929b5c90bfe6f7249db2af4b117916bdd945b7c8
tomashof01 <tomashofstetter2000@gmail.com>
2022-12-14 20:20:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+let port = 3000;
+
+app.use(express.static("public"));
+
+app.get ('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+});
+
+app.get ('/register', (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+
+app.get ('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
+
+app.listen(port, () => console.log('listening in port' + port));
